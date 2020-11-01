bfc1011080c073a68f7867a51922ad9eb0e173f6
Yuansong Zhang <306336548@qq.com>
2020-11-01 14:30:35 +0800

    fix rl_trainer

--- hw1/cs285/infrastructure/rl_trainer.py
+++ hw1/cs285/infrastructure/rl_trainer.py
@@ -179,8 +179,8 @@ class RL_Trainer(object):
         # HINT2: you want each of these collected rollouts to be of length self.params['ep_len']
         print("\nCollecting data to be used for training...")
         # paths, envsteps_this_batch = TODO
-        paths, envsteps_this_batch = utils.sample_trajectories(self.env, collect_policy,
-                                                               self.params['ep_len'], batch_size)
+        paths, envsteps_this_batch = utils.sample_trajectories(self.env, collect_policy, batch_size,
+                                                               self.params['ep_len'])
 
         # collect more rollouts with the same policy, to be saved as videos in tensorboard
         # note: here, we collect MAX_NVIDEO rollouts, each of length MAX_VIDEO_LEN
