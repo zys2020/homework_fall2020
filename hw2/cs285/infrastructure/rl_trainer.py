b9bd906edd06a17221c5d7b050b907d63ee7bc2b
Yuansong Zhang <306336548@qq.com>
2020-11-11 12:24:54 +0800

    implement rl_trainer

--- hw2/cs285/infrastructure/rl_trainer.py
+++ hw2/cs285/infrastructure/rl_trainer.py
@@ -1,11 +1,8 @@
 from collections import OrderedDict
 import pickle
-import os
-import sys
 import time
 
 import gym
-from gym import wrappers
 import numpy as np
 import torch
 from cs285.infrastructure import pytorch_util as ptu
@@ -151,10 +148,39 @@ class RL_Trainer(object):
     def collect_training_trajectories(self, itr, load_initial_expertdata, collect_policy, batch_size):
         # TODO: get this from hw1
         # if your load_initial_expertdata is None, then you need to collect new trajectories at *every* iteration
+        if itr == 0:
+            with open(load_initial_expertdata, 'rb') as f:
+                # the number of training data collected (in the env) during each iteration defaults 1000
+                # so the expert data's shape is [1000, x](e.g., observation's x is 111, action's x is 8 ,etc)
+                paths = pickle.load(f)
+                new_paths = []
+                for path in paths:
+                    for key in path:
+                        path[key] = path[key][:batch_size]
+                    new_paths.append(path)
+            return new_paths, 0, None
+        print("\nCollecting data to be used for training...")
+        paths, envsteps_this_batch = utils.sample_trajectories(self.env, collect_policy, batch_size,
+                                                               self.params['ep_len'])
+
+        # collect more rollouts with the same policy, to be saved as videos in tensorboard
+        # note: here, we collect MAX_NVIDEO rollouts, each of length MAX_VIDEO_LEN
+        train_video_paths = None
+        if self.log_video:
+            print('\nCollecting train rollouts to be used for saving videos...')
+            train_video_paths = utils.sample_n_trajectories(self.env, collect_policy, MAX_NVIDEO, MAX_VIDEO_LEN, True)
         return paths, envsteps_this_batch, train_video_paths
 
     def train_agent(self):
         # TODO: get this from hw1
+        print('\nTraining agent using sampled data from replay buffer...')
+        train_logs = []
+        for train_step in range(self.params['num_agent_train_steps_per_iter']):
+            ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch = self.agent.sample(
+                self.params['train_batch_size'])
+
+            train_log = self.agent.train(ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch)
+            train_logs.append(train_log)
         return train_logs
 
     ####################################
